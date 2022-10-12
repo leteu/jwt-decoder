e3ab4e62770c52efa4942b82f55eb6b8223a5bf9
ghshin <leteu610@gmail.com>
2022-10-12 14:12:09 +0900

    Feat: decodeToken

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-function test () {
-  return 'test'
-}
+import * as buffer from 'buffer'
 
-export default test
+function decodeToken<T>(token: string): T {
+  let base64Url: string;
+
+  if (token.match(/^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$/)) {
+    base64Url = token.split('.')[1];
+  } else if (token.match(/^[A-Za-z0-9-_]*$/)) {
+    base64Url = token;
+  } else {
+    console.log('token is not vaild');
+    return
+  }
+
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const JSONToken = JSON.parse(decodeURIComponent(buffer.Buffer.from(base64, 'base64').toString()));
+
+  return JSONToken;
+};
+
+export {
+  decodeToken
+}
+export default decodeToken;
